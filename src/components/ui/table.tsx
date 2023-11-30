7d214f98b6ccb649639c197ef13b1547ec6443f9
Sammy Ferreira <sammyfe2021@gmail.com>
2023-11-30 09:32:25 -0300

    :bug: fix: prettier shadcn/ui

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
